3a8bcd5178d939e9703de2c3933188410f9dbf04
Mateus Arbex <51658233+mateusarbex@users.noreply.github.com>
2021-10-25 22:20:40 -0400

    added current playback

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import styled from "styled-components";
 import { BsSpotify } from "react-icons/bs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import InputSpotify from "./components/InputSpotify/InputSpotify";
 import Track from "./components/Track/Track";
+import { IoPlayCircleOutline } from "react-icons/io5";
 
 const TracksContainer = styled.div`
   max-width: 100vw;
@@ -27,8 +28,18 @@ const api = axios.create({
 });
 
 function App() {
+  const [currentTrack, setCurrentTrack] = useState();
   const [input, setInput] = useState("");
   const [tracks, setTracks] = useState([]);
+
+  const getCurrentSong = async () => {
+    const current_song = await api.get("playback");
+    setCurrentTrack(current_song.data);
+  };
+
+  useEffect(() => {
+    getCurrentSong();
+  }, []);
 
   const queryInput = async () => {
     if (input) {
@@ -41,16 +52,32 @@ function App() {
   };
 
   const addToQueue = async (uri) => {
-    const response = await api.post("add_to_queue", { uri });
+    const response = await api
+      .post("add_to_queue", { uri })
+      .catch((err) => console.log(err));
     console.log(response);
   };
 
   return (
     <div className="App">
-      <a href="http://localhost:5000/">Link</a>
       <header className="App-header" />
       <SpotifyLogo />
-      <h3>Adicione para a playlist</h3>
+      <div
+        style={{
+          flexDirection: "row",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <h2 style={{ letterSpacing: "3px", marginRight: 10 }}>NOW PLAYING</h2>
+        <IoPlayCircleOutline style={{ height: 24, width: 24 }} />
+      </div>
+      {currentTrack && (
+        <TracksContainer>
+          <Track noIcon={true} track={currentTrack}></Track>
+        </TracksContainer>
+      )}
+
       <InputSpotify
         onBlur={() => queryInput()}
         onInput={({ target }) => setInput(target.value)}
@@ -58,6 +85,7 @@ function App() {
         onIconClick={() => queryInput()}
         placeholder={"Artista ou música"}
       />
+      <h3>Adicione para a playlist</h3>
       <TracksContainer>
         {tracks.map((value) => {
           return (
